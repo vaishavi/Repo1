a91a23a1908eb723fb96937d0d3822bd938d2932
vaishavi <vvaishavi99@gmail.com>
2021-09-15 12:20:15 +0530

    added booking

---
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -0,0 +1,15 @@
+package com.lti.model;
+
+public class Booking {
+
+	int bookingno;
+	Flight flight;
+	public int getBookingno() {
+		return bookingno;
+	}
+	
+	public void setFlight(Flight flight) {
+		this.bookingno=bookingno;
+	}
+
+}
