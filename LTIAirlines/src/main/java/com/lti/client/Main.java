55174efbc7bdd60aae58079e989c19a434f2b4cd
Vaishavi <vvaishavi99@gmail.com>
2021-09-15 10:48:46 +0530

    First commit

---
+++ LTIAirlines/src/main/java/com/lti/client/Main.java
@@ -0,0 +1,17 @@
+package com.lti.client;
+
+import com.lti.model.Flight;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		int num2;
+		
+		
+		
+
+	}
+
+}
---
+++ LTIAirlines/src/test/java/com/lti/test/Testcase1.java
@@ -0,0 +1,19 @@
+package com.lti.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.lti.client.Main;
+
+public class Testcase1 {
+
+	@Test
+	public void testAdd() {
+		int num1=10;
+		int num2=10;
+		
+		//assertEquals(20, Main.add(num1,num2));
+	}
+
+}
